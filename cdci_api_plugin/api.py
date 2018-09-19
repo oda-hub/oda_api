3522d66aaa067a529f6ed51ca8f7285fcde35d5b
tramacer <andrea.tramacere@gmail.com>
2018-09-19 16:01:15 +0200

    improve api

--- cdci_api_plugin/api.py
+++ cdci_api_plugin/api.py
@@ -19,6 +19,14 @@ class Request(object):
         pass
 
 
+class RemoteException(Exception):
+
+    def __init__(self, message='Remote analysis exception', debug_message=''):
+        super(RemoteException, self).__init__(message)
+        self.message=message
+        self.debug_message=debug_message
+
+
 
 class DispatcherAPI(object):
 
@@ -54,11 +62,12 @@ class DispatcherAPI(object):
         if url is None:
             url=self.url
 
-
+        print('waiting for remote response, please wait')
         res= requests.get("%s/%s" %(url, handle), params=parameters_dict)
         query_status = res.json()['query_status']
         job_id = res.json()['job_monitor']['job_id']
-        print ('working remotely, please wait')
+        if query_status != 'done' and query_status != 'failed':
+            print ('the job is working remotely, please wait')
         while query_status != 'done' and query_status != 'failed':
             parameters_dict['query_status']=query_status
             parameters_dict['job_id'] = job_id
@@ -70,31 +79,35 @@ class DispatcherAPI(object):
 
             time.sleep(5)
 
-        if query_status != 'failed':
-            pass
-        else:
+        if  res.json()['exit_status']['status']!=0:
             self.failure_report(res)
-            raise Exception('query failed',)
 
-        print('exit_status, status', res.json()['exit_status']['status'])
-        print('exit_status, message', res.json()['exit_status']['message'])
-        print('exit_status, error_message', res.json()['exit_status']['error_message'])
-        print('exit_status, debug_message', res.json()['exit_status']['debug_message'])
-        print('job_monitor', res.json()['job_monitor'])
-        print('query_status', res.json()['query_status'])
-        print('products', res.json()['products'].keys())
+
+        #print('job_monitor', res.json()['job_monitor'])
+        #print('query_status', res.json()['query_status'])
+        #print('products', res.json()['products'].keys())
+
+        if query_status != 'failed':
+            print('query done succesfully!')
+        else:
+
+            raise RemoteException(debug_message=res.json()['exit_status']['error_message'])
+
+
 
         return res
 
 
     def failure_report(self,res):
-        print('exit_status, status', res.json()['exit_status']['status'])
-        print('exit_status, message', res.json()['exit_status']['message'])
-        print('exit_status, error_message', res.json()['exit_status']['error_message'])
-        print('exit_status, debug_message', res.json()['exit_status']['debug_message'])
+        print('query failed!')
+        #print('exit_status, status', res.json()['exit_status']['status'])
+        print('Remote server message:->', res.json()['exit_status']['message'])
+        print('Remote server error_message->', res.json()['exit_status']['error_message'])
+        print('Remote server debug_message->', res.json()['exit_status']['debug_message'])
 
     def dig_list(self,b,only_prod=False):
         from astropy.table import Table
+        #print ('start',type(b))
         if isinstance(b, (set, tuple, list)):
             for c in b:
                 self.dig_list(c)
@@ -102,12 +115,12 @@ class DispatcherAPI(object):
             #print('not list',type(b))
             try:
                 b = ast.literal_eval(str(b))
-                #print(type(b))
+                #print('b literal eval',(type(b)))
             except:
-                #print ('except')
+                #print ('b exception' ,b,type(b))
                 return str(b)
             if isinstance(b, dict):
-                #print('dict')
+                #print('dict',b)
                 _s = ''
                 for k, v in b.items():
 
@@ -132,56 +145,63 @@ class DispatcherAPI(object):
                 if _s != '':
                     print(_s)
             else:
+                #print('no dict', type(b))
                 self.dig_list(b)
 
 
 
 
 
+    def _decode_res_json(self,res):
+        if hasattr(res,'content'):
+            _js = json.loads(res.content)
+            res = ast.literal_eval(str(_js).replace('null', 'None'))
+        else:
+            res = ast.literal_eval(str(res).replace('null', 'None'))
 
+        self.dig_list(res)
+        return res
 
     def get_instrument_description(self,instrument=None):
         if instrument is None:
             instrument=self.instrument
 
         res=requests.get("%s/api/meta-data"%self.url,params=dict(instrument=instrument))
-        _js = json.loads(res.content)
-        a = ast.literal_eval(str(_js).replace('null', 'None'))
-        self.dig_list(a)
-        #for _d in a[0]:
-        #    if isinstance(_d,list):
-         #       for _d1 in _d:
-          #          print('a',_d1)
-          #  else:
-          #      print ('b',_d,type(_d))
+        self._decode_res_json(res)
+
+
 
 
     def get_product_description(self,instrument,product_name):
         res = requests.get("%s/api/meta-data" % self.url, params=dict(instrument=instrument,product_type=product_name))
-        _js = json.loads(res.content)
-        a = ast.literal_eval(str(_js).replace('null', 'None'))
+
         print('--------------')
         print ('parameters for  product',product_name,'and instrument',instrument)
-        self.dig_list(a)
+        self._decode_res_json(res)
+
+
 
     def get_instruments_list(self):
         #print ('instr',self.instrument)
         res = requests.get("%s/api/instr-list" % self.url,params=dict(instrument=self.instrument))
-        _js = json.loads(res.content)
-        a = ast.literal_eval(str(_js).replace('null', 'None'))
-        self.dig_list(a)
-        return a
+        return self._decode_res_json(res)
 
 
-    def get_product(self,product,instrument ,asynch=True, **kwargs):
+
+    def get_product(self,product,instrument ,verbose=False,dry_run=False, **kwargs):
         kwargs['instrument'] = instrument
         kwargs['product_type'] = product
         kwargs['query_type'] = 'Real'
         kwargs['off_line'] = False,
-        kwargs['run_asynch'] = asynch,
         kwargs['query_status'] = 'new',
+        kwargs['verbose'] = verbose,
         kwargs['session_id'] = self.generate_session_id()
+        kwargs['dry_run'] = dry_run,
+
         res = self.request(kwargs)
 
-        return res.json()['products']['data']
+        if dry_run  ==False:
+            return res.json()['products']['data']
+        else:
+            self._decode_res_json(res.json()['products']['instrumet_parameters'])
 
