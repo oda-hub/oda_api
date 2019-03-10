f9382c32aa6aa6afedb91223d8f191f58e74bbc8
tramacer <andrea.tramacere@gmail.com>
2019-03-10 19:50:08 +0100

    debug doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,8 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
-import oda_api
+sys.path.insert(0, os.path.abspath('../../'))
+#import oda_api
 
 
 # -- Project information -----------------------------------------------------
