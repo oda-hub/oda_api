f8da74a23c275f5998e9daa52bc78c685ca5378e
tramacer <andrea.tramacere@gmail.com>
2019-03-11 09:09:19 +0100

    update doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,13 +41,6 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [ 'sphinx.ext.autodoc'
-               'sphinx.ext.intersphinx',
-               'sphinx.ext.todo',
-               'sphinx.ext.coverage',
-               'sphinx.ext.ifconfig',
-               'sphinx.ext.viewcode',
-               'sphinx.ext.autosummary',
-               'sphinx.ext.graphviz',
                'sphinx_automodapi.automodapi',
                'sphinx.ext.inheritance_diagram',
                'sphinx.ext.autosummary',
