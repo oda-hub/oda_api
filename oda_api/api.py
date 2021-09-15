8b2c31604c92a4ca5a563e3a0e384b0474a1b947
burnout87 <gabriele.barni@gmail.com>
2021-09-15 09:58:59 +0200

    additional warning

--- oda_api/api.py
+++ oda_api/api.py
@@ -850,6 +850,10 @@ class DispatcherAPI:
         kwargs['verbose'] = verbose,
         kwargs['session_id'] = self.session_id
 
+        if 'dry_run' in kwargs:
+            warnings.warn('The dry_run parameter you included is not going to have any effect on the execution.\n'
+                          'However the oda_api will perform a check of the list opf valid parameters for your request.')
+
         res = requests.get("%s/api/par-names" % self.url, params=dict(
             instrument=instrument, product_type=product), cookies=self.cookies)
 
