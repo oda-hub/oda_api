6d94af984ab7277134d0da4dbb57beeb3fd5cc89
burnout87 <gabriele.barni@gmail.com>
2022-08-04 10:40:35 +0200

    mjd to utc conversion function and adapted test

--- oda_api/api.py
+++ oda_api/api.py
@@ -991,13 +991,13 @@ class DispatcherAPI:
         # validate/parse t1 and t2
         if 'T1' in kwargs:
             if type(kwargs['T1']) == int or type(kwargs['T1']) == float:
-                t1_utc = self.convert_ijd_to_utc(kwargs['T1'] - 51544)
+                t1_utc = self.convert_mijd_to_utc(kwargs['T1'])
                 logger.info("The value of T1 has been provided in a difference format from UTC, "
                             "this will be attempted to be converted as such before being uploaded over the gallery")
                 copied_kwargs['T1'] = t1_utc
         if 'T2' in kwargs:
             if type(kwargs['T2']) == int or type(kwargs['T2']) == float:
-                t2_utc = self.convert_ijd_to_utc(kwargs['T2'] - 51544)
+                t2_utc = self.convert_mijd_to_utc(kwargs['T2'])
                 logger.info("The value of T2 has been provided in a difference format from UTC, "
                             "this will be attempted to be converted as such before being uploaded over the gallery")
                 copied_kwargs['T2'] = t2_utc
@@ -1157,6 +1157,11 @@ class DispatcherAPI:
         # TODO to reply on a dedicated service in the dispatcher
         res = requests.get(f"https://www.astro.unige.ch/mmoda/dispatch-data/gw/timesystem/api/v1.0/converttime/IJD/{t_ijd}/UTC")
         t_utc = res.text
+        return t_utc
+
+    def convert_mijd_to_utc(self, t_mijd):
+        # TODO to reply on a dedicated service in the dispatcher
+        t_utc = self.convert_ijd_to_utc(t_mijd - 51544)
         return t_utc
 
     def check_gallery_data_product_policy(self,
