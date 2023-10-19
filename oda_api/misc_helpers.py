68a7c86412bc17a0c60cf82fca029f6282a3a1ed
Denys SAVCHENKO <savchenko@apc.in2p3.fr>
2023-10-19 16:47:04 +0200

    better url validation regex

--- oda_api/misc_helpers.py
+++ oda_api/misc_helpers.py
@@ -5,15 +5,13 @@ from collections import OrderedDict
 
 regex_url = re.compile(
                 r'^https?://'  # http:// or https://
-                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-                r'localhost|'  # localhost...
-                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+                r'[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*'  # domain...
                 r'(?::\d+)?'  # optional port
-                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+                r'(?:/[a-z-/]*)?$', re.IGNORECASE)
 
 
 def validate_url(url):
-    return re.match(regex_url, url) is not None
+    return regex_url.match(url) is not None
 
 
 def clean_var_name(s):
