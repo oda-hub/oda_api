fea3dc85745db0a49d19cf44a24e690bf0dc09b2
Denys SAVCHENKO <savchenko@apc.in2p3.fr>
2023-10-24 18:12:09 +0200

    silence mypy no-redef

--- oda_api/ontology_helper.py
+++ oda_api/ontology_helper.py
@@ -9,7 +9,7 @@ try:
     from cdci_data_analysis.analysis.exceptions import RequestNotUnderstood
     # special treatment of this exception when working with dispatcher 
 except ImportError:
-    class RequestNotUnderstood(RuntimeError): pass
+    class RequestNotUnderstood(RuntimeError): pass # type: ignore[no-redef]
 
 
 logger = logging.getLogger(__name__)
