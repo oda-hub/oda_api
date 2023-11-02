5e3aa218224dfe0ac089dde09c3d9ba4727ab964
burnout87 <gabriele.barni@gmail.com>
2023-11-02 17:39:19 +0100

    adapt test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -71,9 +71,9 @@ def test_get(dispatcher_live_fixture, caplog, monkeypatch, tmpdir):
     result = runner.invoke(cli.cli, ['-u', dispatcher_live_fixture, 'get'], obj={})
     assert result.exit_code == 0
 
-    assert "found instruments: ['empty', 'empty-async', 'empty-semi-async']" in caplog.text or \
-           "found instruments: ['empty', 'empty-async', 'empty-semi-async', 'isgri', 'jemx', 'osa_fake']" in caplog.text or \
-           "found instruments: ['isgri', 'jemx', 'osa_fake', 'empty', 'empty-async', 'empty-semi-async']" in caplog.text
+    assert "found instruments: ['empty', 'empty-async', 'empty-semi-async', 'empty-async-return-progress']" in caplog.text or \
+           "found instruments: ['empty', 'empty-async', 'empty-semi-async', 'empty-async-return-progress', 'isgri', 'jemx', 'osa_fake']" in caplog.text or \
+           "found instruments: ['isgri', 'jemx', 'osa_fake', 'empty', 'empty-async', 'empty-async-return-progress', 'empty-semi-async']" in caplog.text
 
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['-u', dispatcher_live_fixture, 'get', '-i', 'empty'], obj={})
