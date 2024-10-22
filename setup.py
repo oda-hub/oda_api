cc250103c3bcd3add1399b1b52c23306ee67540b
burnout87 <gabriele.barni@gmail.com>
2024-10-22 13:33:43 +0200

    using toml file

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-#!/usr/bin/env python
-
-from __future__ import absolute_import, division, print_function
-
-from builtins import (bytes, str, open, super, range,
-                      zip, round, input, int, pow, object, map, zip)
-
-__author__ = 'andrea tramacere'
-
-
-from distutils.core import setup
-import glob
+from setuptools import setup
 import json
 
 with open('oda_api/pkg_info.json') as fp:
@@ -17,56 +6,7 @@ with open('oda_api/pkg_info.json') as fp:
 
 __version__ = _info['version']
 
-
-include_package_data=True
-
-setup(name='oda_api',
-      version=__version__,
-      description='API plugin  for CDCI online data analysis',
-      author='Andrea Tramacere, Volodymyr Savchenko',
-      author_email='contact@odahub.io',
-      packages=['oda_api'],
-      package_data={'oda_api': ['config_dir/*']},
-      include_package_data=True,
-      install_requires=[
-            "requests",
-            "future",
-            "astropy>=3.2",
-            "json_tricks",
-            "matplotlib",
-            "numpy",
-            "jsonschema",
-            "pyjwt",
-            "astroquery",
-            "scipy",
-            "rdflib",
-            "black"
-        ],
-      extras_require={
-          'test': [
-                "pytest-xdist[psutil]",
-                "astroquery>=0.4.4",
-                "sentry_sdk"
-            ],      
-          'extra-test': [
-                "pytest-xdist[psutil]",
-                "astroquery>=0.4.4",
-            ],      
-          'gw': [
-            "gwpy",
-            "ligo.skymap"
-          ],
-          'ontology': [
-            "rdflib"
-          ]
-         },
-      entry_points={
-          "console_scripts": [
-            "oda-api = oda_api.cli:main"
-          ]
-        },
-      python_requires='>=2.7',
-      )
-
-
-
+setup(
+    version=__version__,
+    setup_requires=['setuptools'],
+)
